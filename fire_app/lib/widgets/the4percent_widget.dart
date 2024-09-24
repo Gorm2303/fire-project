9cdd5b1a47bcd1b9e5b5116d7a14eca0be5c0df7
Gorm2303 <gormdk333@gmail.com>
2024-09-24 23:28:27 +0200

    Update the4percent_widget.dart

--- fire_app/lib/widgets/the4percent_widget.dart
+++ fire_app/lib/widgets/the4percent_widget.dart
@@ -10,6 +10,7 @@ class The4PercentWidget extends StatelessWidget {
   final TextEditingController breakController;
   final double compoundGatheredDuringBreak; 
   final VoidCallback onInputChanged;  // Callback for when any input changes
+  final TextEditingController withdrawalTimeController;
 
   const The4PercentWidget({
     super.key,
@@ -22,61 +23,68 @@ class The4PercentWidget extends StatelessWidget {
     required this.breakController,
     required this.compoundGatheredDuringBreak,
     required this.onInputChanged,
+    required this.withdrawalTimeController,
   });
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
+        // Constrain the Break Period Input to a fixed width
+        SizedBox(
+          width: 305,  // Set the fixed width for the TextField
+          child: TextField(
+            controller: breakController,
+            decoration: const InputDecoration(labelText: 'Break Period (No Deposits Nor Withdrawals in Years)'),
+            keyboardType: TextInputType.number,
+            onChanged: (value) => onInputChanged(),  // Trigger recalculation on change
+          ),
+        ),
+        const SizedBox(height: 15),  // Spacing between rows
+        // Constrain the Compound Gathered Text
+        Text(
+            'Compound Gathered During Break: ${compoundGatheredDuringBreak.toStringAsFixed(0)} kr.-',
+            style: const TextStyle(fontSize: 16),
+          ),
+        const SizedBox(height: 15),  // Spacing between rows
+        SizedBox(
+          width: 305,  // Set the fixed width for the TextField
+          child: TextField(
+            controller: withdrawalTimeController,
+            decoration: const InputDecoration(labelText: 'Withdrawal Period (Years)'),
+            keyboardType: TextInputType.number,
+            onChanged: (value) => recalculateValues(),  // Trigger recalculation on change
+          ),
+        ),
+        const SizedBox(height: 15),  // Spacing between rows
         Row(
-          children: [
-            // Break Period Input
-            Expanded(
-              child: TextField(
-                controller: breakController,
-                decoration: const InputDecoration(labelText: 'Break Period After Investing Time (No Deposits Nor Withdrawals in Years)'),
-                keyboardType: TextInputType.number,
-                onChanged: (value) => onInputChanged(),  // Trigger recalculation on change
-              ),
-            ),
-            const SizedBox(width: 16),  // Add some spacing between TextField and the Text
-            // Display Compound Interest Gathered During Break Period
-            Expanded(
-              child: Text(
-                'Compound Gathered: ${compoundGatheredDuringBreak.toStringAsFixed(0)} kr.-',
+            mainAxisAlignment: MainAxisAlignment.center,  // Center the row
+            children: <Widget>[
+              // Constrain the Withdrawal Period Input to a fixed width
+                DropdownButton<String>(
+                  value: withdrawalPercentageController.text,
+                  items: ['3', '4', '5'].map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text('$value%'),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    if (newValue != null) {
+                      withdrawalPercentageController.text = newValue;
+                      recalculateValues();  // Trigger recalculation when value changes
+                    }
+                  },
+                ),
+              // Constrain the Withdrawal Each Month Text
+              Text(
+                'Withdrawal Each Month: ${(customWithdrawalRule / 12).toStringAsFixed(0)} kr.-',
                 style: const TextStyle(fontSize: 16),
+                softWrap: true, // Allow text wrapping
               ),
-            ),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // Dropdown for selecting withdrawal percentage (3%, 4%, 5%)
-            DropdownButton<String>(
-              value: withdrawalPercentageController.text,
-              items: ['3', '4', '5'].map((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text('$value%'),
-                );
-              }).toList(),
-              onChanged: (String? newValue) {
-                if (newValue != null) {
-                  withdrawalPercentageController.text = newValue;
-                  recalculateValues();  // Trigger recalculation when value changes
-                }
-              },
-            ),
-            const SizedBox(width: 16),
-            // Display the monthly withdrawal amount
-            Text(
-              'Withdrawal Each Month: ${(customWithdrawalRule / 12).toStringAsFixed(0)} kr.-',
-              style: const TextStyle(fontSize: 16),
-            ),
-          ],
-        ),
-        const SizedBox(height: 16),
+            ],
+          ),
+        const SizedBox(height: 10),  // Spacing between rows
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -99,6 +107,11 @@ class The4PercentWidget extends StatelessWidget {
             ),
           ],
         ),
+        const SizedBox(height: 15),  // Spacing between rows
+        Text(
+              'Monthly Withdrawal After Tax: ${((customWithdrawalRule / 12) - customWithdrawalTax / 12).toStringAsFixed(0)} kr.-',
+              style: const TextStyle(fontSize: 16),
+            ),
       ],
     );
   }
